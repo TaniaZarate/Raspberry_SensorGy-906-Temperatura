d3e64a9278f5bd85fd42c23c2b709a848ef840cf
Tania Zárate <90163838+TaniaZarate@users.noreply.github.com>
2022-01-15 14:22:32 -0600

    Add files via upload

---
+++ TemperaturaI2C.py
@@ -0,0 +1,10 @@
+#Sensor Temperatura I2C
+
+from smbus2 import SMBus
+from mlx90614 import MLX90614
+
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
+print ("Temperatura ambiente :", sensor.get_ambient())
+print ("Temperatura de Persona u objeto :", sensor.get_object_1())
+bus.close() 
